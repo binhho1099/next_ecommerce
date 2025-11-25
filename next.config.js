2e1a74915fa3ba36ec7ed88b9577846ff3120dfb
Bình Hồ <binh.ht@terasolutions.vn>
2025-11-25 22:28:22 +0700

    fix: config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/e-commerce',
+  assetPrefix: '/e-commerce/',
   reactStrictMode: true,
   images: {
     domains: [
       'i.dummyjson.com',
-      'cdn.dummyjson.com',    // thêm dòng này
+      'cdn.dummyjson.com',
       'robohash.org',
       'graph.facebook.com',
       'lh3.googleusercontent.com',
