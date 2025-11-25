8505c21222bbedf45bbbae62727650c83f64383f
Bình Hồ <binh.ht@terasolutions.vn>
2025-11-25 22:01:36 +0700

    feat: config nextjs deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/e-commerce',
   reactStrictMode: true,
   images: {
     domains: [
       'i.dummyjson.com',
+      'cdn.dummyjson.com',    // thêm dòng này
       'robohash.org',
       'graph.facebook.com',
       'lh3.googleusercontent.com',
